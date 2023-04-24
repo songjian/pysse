d17b0d6cb64e4fc7b2aaef33e1dcea73e74941ad
sj <songjian@codeorder.cn>
2023-04-24 16:45:55 +0800

    添加测试

--- tests/stock_test.py
+++ tests/stock_test.py
@@ -1,12 +1,3 @@
-# import unittest
-# from sse.stock import overview,profile
-
-# class StockTestCase(unittest.TestCase):
-#     def test_overview(self):
-#         date = '20220323'
-#         self.assertEqual(overview(date), 4)
-
-
 import unittest
 from sse.stock import Stock
 import pandas as pd
