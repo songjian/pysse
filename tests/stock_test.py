29d1e8321745a8f98de9605b4f86d3e4e3003529
sj <songjian@codeorder.cn>
2023-04-24 16:45:33 +0800

    添加测试

--- tests/stock_test.py
+++ tests/stock_test.py
@@ -1,7 +1,32 @@
-import unittest
-from sse.stock import overview,profile
+# import unittest
+# from sse.stock import overview,profile
 
-class StockTestCase(unittest.TestCase):
+# class StockTestCase(unittest.TestCase):
+#     def test_overview(self):
+#         date = '20220323'
+#         self.assertEqual(overview(date), 4)
+
+
+import unittest
+from sse.stock import Stock
+import pandas as pd
+
+class TestSSE(unittest.TestCase):
+    def setUp(self):
+        self.sse = Stock()
+
+    def test_stock_list(self):
+        stock_type = '1'
+        result = self.sse.stock_list(stock_type)
+        self.assertIsInstance(result, pd.DataFrame)
+        self.assertTrue(len(result) > 0)
+        
     def test_overview(self):
         date = '20220323'
-        self.assertEqual(overview(date), 4)
+        result = self.sse.overview(date)
+        self.assertIsInstance(result, pd.DataFrame)
+        self.assertTrue(len(result) > 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
